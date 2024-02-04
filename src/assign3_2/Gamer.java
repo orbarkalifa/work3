4834d6ff771a85ba010e5bc52fe7816e0c449623
Or Bar Califa <orbarkalifa@gmail.com>
2024-02-04 12:10:54 +0200

    minor fixes

--- src/assign3_2/Gamer.java
+++ src/assign3_2/Gamer.java
@@ -3,15 +3,14 @@ package assign3_2;
 public class Gamer extends Thread{
 
     private int goodFlipsCounter = 0;
-    public boolean interrupted = false;
     public GamePlay game;
 
     public Gamer(GamePlay game){
         this.game = game;
     }
 
-    public void play(){
-        while(!interrupted && game.getNumOfRounds() < 10){
+    public synchronized void play(){
+        while(!Thread.currentThread().isInterrupted() && game.getNumOfRounds() < 10){
             if (game.flipCoin()) goodFlipsCounter++;
             try {
                 sleep(1000);
