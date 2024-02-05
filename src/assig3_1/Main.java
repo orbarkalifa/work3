211bc0a314d3059fb35cf0952789f949f045fca4
Or Bar Califa <orbarkalifa@gmail.com>
2024-02-05 20:49:31 +0200

    q1 working (maybe)

--- src/assig3_1/Main.java
+++ src/assig3_1/Main.java
@@ -1,22 +1,27 @@
 package assig3_1;
 
+import static java.lang.Thread.sleep;
 
 class T1 implements Runnable {
-
     @Override
     public void run() {
         while (true) {
-            while (Main.turn != 1){
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            synchronized (Main.lock) {
+                while (Main.turn != 1) {
+                    try {
+                        Main.lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+
+                System.out.print(" A ");
+                Main.turn = 2;
+                Main.lock.notifyAll();
+                try{
+                    sleep(50);
+                }catch (Exception ignored){}
             }
-
-            System.out.print(" A ");
-            Main.turn = 2;
-
         }
     }
 }
@@ -24,44 +29,55 @@ class T1 implements Runnable {
 class T2 implements Runnable {
     @Override
     public void run() {
-        while(true){
-            while (Main.turn == 1 || Main.turn == 4){
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+        while (true) {
+            synchronized (Main.lock) {
+                while (Main.turn == 1 || Main.turn == 4) {
+                    try {
+                        Main.lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }
-            System.out.print(" B ");
-                Main.turn = 3;
 
+                System.out.print(" B ");
+                Main.turn = 3;
+                Main.lock.notifyAll();
+                try{
+                    sleep(50);
+                }catch (Exception ignored){}
+            }
         }
     }
 }
+
 class T3 implements Runnable {
     @Override
     public void run() {
         while (true) {
-            while (Main.turn != 3){
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            synchronized (Main.lock) {
+                while (Main.turn != 3) {
+                    try {
+                        Main.lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }
+
                 Main.turn = 4;
                 System.out.print(" C ");
                 Main.turn = 1;
-
-
+                Main.lock.notifyAll();
+                try{
+                    sleep(50);
+                }catch (Exception ignored){}
+            }
         }
     }
 }
 
 public class Main {
-
     public static Integer turn = 1;
-
+    public static final Object lock = new Object();
 
     public static void main(String[] args) {
         Thread t1 = new Thread(new T1());
@@ -70,8 +86,5 @@ public class Main {
         t1.start();
         t2.start();
         t3.start();
-
-
-
     }
 }
